1346f6194adcd84c864b1800f19f7b9ac37083ef
HorizontalSpa <m.nikulin23@mail.ru>
2026-07-11 19:41:27 +0300

    Add localization functions for language detection and content

--- src/utils/appLocalization.js
+++ src/utils/appLocalization.js
@@ -1,1 +1,11 @@
-
+export function detectLanguage() { return 'ru'; }
+export function loadContent() {
+  return {
+    welcomeCard: { subtext: 'Сеть массажных студий в Москве', buttonText: 'Перейти в каталог' },
+    home: {},
+    benefit: {},
+    ranking: {},
+    profile: {},
+    desktop: {}
+  };
+}
